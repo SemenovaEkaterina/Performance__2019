78d707d74e22f8c56d081fa7b452095aacb32555
Kate Semenova <e.semenova@ktsstudio.ru>
2019-10-16 14:56:35 +0300

    replace widgets

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const multimediaScript = document.createElement('script');
-    multimediaScript.setAttribute('src','./js/multimedia.js');
+    (function(w, d, n) {
+        var s = d.createElement('script');
+        s.src = '//zen.yandex.ru/widget-loader';
+        d.head.appendChild(s);
+        var c = d.createElement('div');
+        c.style.display = 'none';
+        d.body.appendChild(c);
+        w[n] = w[n] || [];
+        w[n].push(function() {
+            w.YandexZen.renderWidget({
+                container: c,
+                clid: [123,123],
+            });
+        })
+    })(window, document, 'yandexZenAsyncCallbacks');
+
     const buttonsContainer = document.querySelector('.buttons-wrap');
 
     buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
         '<button class="button">Нет</button>';
-    
-    document.querySelector('.multimedia').appendChild(multimediaScript);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
