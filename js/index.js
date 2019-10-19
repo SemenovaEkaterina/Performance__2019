896d701fbe65e0e45edf4e3c09ab5829e02fdb62
Kate Semenova <e.semenova@ktsstudio.ru>
2019-10-19 05:45:16 +0300

    return

--- js/index.js
+++ js/index.js
@@ -1,31 +1,14 @@
-var DOMReady = function(callback) {
-    if (document.readyState === "interactive" || document.readyState === "complete") {
-        callback();
-    } else if (document.addEventListener) {
-        document.addEventListener('DOMContentLoaded', callback());
-    } else if (document.attachEvent) {
-        document.attachEvent('onreadystatechange', function() {
-            if (document.readyState != 'loading') {
-                callback();
-            }
-        });
-    }
+const cBtn = document.querySelector(".buttons-wrap .button_yellow");
+const btns = cBtn.parentNode;
+const fr = document.querySelector(".card_size_m:nth-child(8) .card-description");
+
+const list = document.createElement('div');
+list.setAttribute('class', 'purchase-list-wrap');
+list.innerHTML = '<p class="card-description card-description_big description_critical">Список покупок:</p>' +
+    '<ol class="purchase-list"><li class="purchase-list__item">Хлеб</li><li class="purchase-list__item">Молоко</li></ol>';
+
+
+cBtn.onclick = () => {
+    fr.replaceWith(list);
+    btns.style.display = "none";
 };
-
-DOMReady(function() {
-    const buttonsContainer = document.querySelector(".buttons-wrap");
-    const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
-
-    const list = document.createElement('div');
-    list.setAttribute('class', 'purchase-list-wrap');
-    list.innerHTML = '<p class="card-description card-description_big description_critical">Список покупок:</p>' +
-        '<ol class="purchase-list"><li class="purchase-list__item">Хлеб</li><li class="purchase-list__item">Молоко</li></ol>';
-
-
-    const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
-
-    confirmPurchaseButton.onclick = () => {
-        fridgeInfoContainer.replaceWith(list);
-        buttonsContainer.style.display = "none";
-    };
-});
